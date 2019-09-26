7285ad4a5feeed81c3bd94266c988709e0387d50
krische777 <kokobluewinkoko@gmail.com>
2019-09-26 12:40:27 +0200

    Edit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Sequelize=require('sequelize');
-const databaseURL = process.env.databaseURL || 'postgres://postgres:secret@localhost:5432/postgres'
+const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const sequelize=new Sequelize(databaseURL);
 
 const express=require('express');
